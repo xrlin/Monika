beff1afef1d11cca9770b1f6b833cd181b87ecd6
xrlin <xrlin@m.scnu.edu.cn>
2019-07-20 17:02:00 +0800

    Add tests and CI.

--- test/controllers/postss_controller_test.rb
+++ test/controllers/postss_controller_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class PostsControllerTest < ActionDispatch::IntegrationTest
   include SignInHelper
   test "new require login" do
-    get new_article_path
+    get new_post_path
     assert_response :redirect
   end
 
   test "edit require login" do
-    get edit_article_path articles(:article1)
+    get edit_post_path posts(:post1)
     assert_response :redirect
   end
 
   test 'get new path success' do
     sign_in_as users(:user1), "password"
-    get new_article_path
+    get new_post_path
     assert_response :ok
   end
 
   test 'edit new path success' do
     sign_in_as users(:user1), "password"
-    get edit_article_path articles(:article1)
+    get edit_post_path posts(:post1)
     assert_response :ok
   end
 
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include SignInHelper
+
+  test "vote require login" do
+    article = articles(:article1)
+    post votes_path(essay_id: article.id, essay_type: article.class.name), as: :json
+    ret = JSON.parse @response.body
+    assert ret['error'].present?
+  end
+
+  test 'cannot vote twice' do
+    article = articles(:article1)
+    user = users(:user1)
+    sign_in_as user, 'password'
+    assert_difference 'article.weighted_score', +1 do
+      post votes_path(essay_id: article.id, essay_type: article.class.name), as: :json
+      article.reload
+    end
+
+    assert_no_changes 'article.weighted_score' do
+      post votes_path(essay_id: article.id, essay_type: article.class.name), as: :json
+      article.reload
+    end
+  end
 end
